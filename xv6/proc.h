765682f9b82d8fdd35f45ee91605ac73c05b69c1
Natasha Orie <norie001@sledge.cs.ucr.edu>
2018-03-03 16:29:01 -0800

    added pg field in proc.h

--- xv6/proc.h
+++ xv6/proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int pg;					   // keeps track of number of pages in stack
 };
 
 // Process memory is laid out contiguously, low addresses first:
